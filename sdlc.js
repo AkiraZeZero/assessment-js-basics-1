12e5925db445d7439b8fe02aecb952436e3e59c3
Kiara Ocasio <kiara.ocasio07@gmail.com>
2022-07-15 11:28:47 -0400

    initial commit

---
+++ sdlc.js
@@ -0,0 +1,38 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+
+const planning = ("This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added.")
+console.log(planning);
+
+const design = ("The list of requirements that you develop in the planning phase is used to make design choices. In the design phase, one or more designs are created to achieve the project result.")
+console.log(design);
+
+const analysisOfRequirements = ("The analysis stage includes gathering all the specific details required for a new system as well as determining the first ideas for prototypes.")
+console.log(analysisOfRequirements);
+
+const testingAndIntegration = ("The objective of this phase is to perform system integration testing of the developed system. The systems integration test function is to ensure that the developed systems meet all the technical requirements with the components and subsystems integrated.")
+console.log(testingAndIntegration);
+
+const implementation = ("This phase is initiated after the system has been tested and accepted by the user. In this phase, the system is installed to support the intended business functions.")
+console.log(implementation);
+
+const maintain = ("This step occurs after the product is in full operation. Maintenance of software can include software upgrades, repairs, and fixes of the software if it breaks.")
+console.log(maintain);
